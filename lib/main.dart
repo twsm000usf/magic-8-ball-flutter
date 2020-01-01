6ed3a3cc28572f9d92f11e250508471a65097c67
Thomas Weverton dos Santos Moura <thomas.moura@mail.usf.edu.br>
2019-12-31 23:11:23 -0300

    Add State and functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,56 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              setState(() {});
+            },
+            child: Image.asset(getBallImageAssetPath()),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+int getRandomBallNumber() {
+  return Random().nextInt(5) + 1;
+}
+
+String getBallImageAssetPath() {
+  int number = getRandomBallNumber();
+  return 'images/ball$number.png';
+}
